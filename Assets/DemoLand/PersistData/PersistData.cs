892daa18fe4e2067f29ba3189005a0140a469941
Andras Serfozo <subztep@gmail.com>
2017-11-11 22:45:25 +0000

    First

---
+++ Assets/DemoLand/PersistData/PersistData.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace DemoLand.PersistData
+{
+	public class PersistData : Singleton<PersistData>
+	{
+		private Hashtable data = new Hashtable();
+
+		/// <summary>
+		/// Set or update data
+		/// </summary>
+		/// <param name="key">Object's identifier</param>
+		/// <param name="obj">The data</param>
+		/// <returns></returns>
+		public void Set(object key, object obj) {
+			if (data.ContainsKey(key)) {
+				data[key] = obj;
+			} else {
+				data.Add(key, obj);
+			}
+		}
+
+		public object Get(object key) {
+			if (data.ContainsKey(key)) {
+				return data[key];
+			}
+			return null;
+		}
+
+		public void Del(object key) {
+			if (data.ContainsKey(key)) {
+				data.Remove(key);
+			}
+		}
+
+		public bool Has(object key) {
+			return data.ContainsKey(key);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+PersistData
+===========
+
+Persistent store data at runtime in Unity3D. Hashmap behind the scenes, accept any kind of types. Singleton, keep data alive between scenes.
+
+> It's under development, even I try to make it good please use it at your own risk.
+
+## Usage example
+
+Set data
+
+```csharp
+PersistData.Instance.Set("test", "data");
+```
+
+Get data
+
+```csharp
+string data = (string)PersistData.Instance.Get("test");
+```
+
+Test data
+
+```cshart
+if (PersistData.Instance.Has("test")) {
+	// Exists
+}
+```
+
+Delete data
+
+```csharp
+PersistData.Instance.Del("test");
+```
